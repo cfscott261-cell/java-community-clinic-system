c6522e477a066a9c2ec1f7e67f87b90c2ea4f22a
mkbrown261 <aviatorkeyz@icloud.com>
2026-02-22 13:12:09 -0500

    customerclass

---
+++ src/main/java/org/codedifferently/Customer.java
@@ -0,0 +1,41 @@
+package org.codedifferently;
+public class Customer {
+}
+
+
+public class Customer {
+
+    private static int nextId = 1;
+
+    private int id;
+    private String name;
+    private boolean checkedIn;
+
+    public Customer(String name) {
+        this.id = nextId++;
+        this.name = name;
+        this.checkedIn = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isCheckedIn() {
+        return checkedIn;
+    }
+
+    public void checkIn() {
+        checkedIn = true;
+    }
+
+    @Override
+    public String toString() {
+        String status = checkedIn ? "Checked In" : "Not Checked In";
+        return "ID: " + id + " | Name: " + name + " | " + status;
+    }
+}
