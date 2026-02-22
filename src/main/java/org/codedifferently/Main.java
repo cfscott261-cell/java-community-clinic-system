3714a26cf30577d3aadcfa2fa071b73e7173235d
Corey Scott <cfscott261@gmail.com>
2026-02-22 12:44:59 -0500

    Changes made

--- src/main/java/org/codedifferently/Main.java
+++ src/main/java/org/codedifferently/Main.java
@@ -1,17 +1,90 @@
-package org.codedifferently;
+package masonbrown;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import java.util.Scanner;
+
+public class MasonBrownCoffeeShopApp {
+
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        MasonBrownCoffeeShopSystem system =
+                new MasonBrownCoffeeShopSystem();
+
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("\n=== Coffee Shop Pickup System ===");
+            System.out.println("1. Add Customer");
+            System.out.println("2. View Customers");
+            System.out.println("3. Check In Customer");
+            System.out.println("4. Schedule Pickup");
+            System.out.println("5. View Schedule");
+            System.out.println("6. Complete Order");
+            System.out.println("7. Daily Summary");
+            System.out.println("0. Exit");
+            System.out.print("Choose an option: ");
+
+            choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+
+                case 1:
+                    System.out.print("Enter name: ");
+                    String name = scanner.nextLine();
+
+                    System.out.print("Enter phone number: ");
+                    String phone = scanner.nextLine();
+
+                    system.addCustomer(name, phone);
+                    break;
+
+                case 2:
+                    system.viewAllCustomers();
+                    break;
+
+                case 3:
+                    System.out.print("Enter Customer ID: ");
+                    int checkId = scanner.nextInt();
+                    scanner.nextLine();
+                    system.checkInCustomer(checkId);
+                    break;
+
+                case 4:
+                    System.out.print("Enter Customer ID: ");
+                    int custId = scanner.nextInt();
+                    System.out.print("Enter Pickup Slot (0-7): ");
+                    int slot = scanner.nextInt();
+                    scanner.nextLine();
+                    system.scheduleOrder(custId, slot);
+                    break;
+
+                case 5:
+                    system.viewSchedule();
+                    break;
+
+                case 6:
+                    System.out.print("Enter Order ID: ");
+                    int orderId = scanner.nextInt();
+                    scanner.nextLine();
+                    system.completeOrder(orderId);
+                    break;
+
+                case 7:
+                    system.dailySummary();
+                    break;
+
+                case 0:
+                    System.out.println("Exiting system...");
+                    break;
+
+                default:
+                    System.out.println("Invalid option.");
+            }
+
+        } while (choice != 0);
+
+        scanner.close();
     }
 }
+
